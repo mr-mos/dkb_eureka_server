317c8a057aa233a6ae8f298565e4988fdb956db3
mos <mos.cheele+github@gmail.com>
2021-06-08 10:40:32 +0200

    Initial commit

---
+++ src/main/java/de/moscon/dkb_eureka_server/DkbEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package de.moscon.dkb_eureka_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DkbEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DkbEurekaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/moscon/dkb_eureka_server/DkbEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.moscon.dkb_eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DkbEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
