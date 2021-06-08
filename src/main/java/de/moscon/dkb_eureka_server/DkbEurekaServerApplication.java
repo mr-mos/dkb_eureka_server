d7013a67253e304c80d9c02fb324707216526e22
mos <mos.cheele+github@gmail.com>
2021-06-08 16:23:49 +0200

    doc + server name

--- src/main/java/de/moscon/dkb_eureka_server/DkbEurekaServerApplication.java
+++ src/main/java/de/moscon/dkb_eureka_server/DkbEurekaServerApplication.java
@@ -6,6 +6,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /*
      https://github.com/spring-guides/gs-service-registration-and-discovery
+
+     Status-Page: http://localhost:8761/
  */
 
 @EnableEurekaServer
